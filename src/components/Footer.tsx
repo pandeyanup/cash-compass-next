3044f3754b30dad97bd82b81120e173a50997738
Anup <pandeyanup58@gmail.com>
2025-01-02 09:14:13 +0545

    fix: upload

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 
 const Footer = () => {
-  return (
+ 
+console.log("Neko");
+  
+    return (
     <footer className="border-t border-gray-200 bg-white">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
